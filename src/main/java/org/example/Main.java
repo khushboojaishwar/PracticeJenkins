7d2c9072ba1c841d420fb4f1ec984a7966a7cd5c
Khushboo jaishwar <kjaishwar25@gmail.com>
2025-09-20 16:46:34 +0530

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.util.Date;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
         System.out.println("hello from java program");
+        System.out.println("Date....." + new Date());
     }
 }
